e010224d16224e8733f1458d9ced126fcbd927de
Anyushu <suzuki@lamp.jp>
2020-12-14 15:27:15 +0900

    update

--- database/seeds/ManagesTableSeeder.php
+++ database/seeds/ManagesTableSeeder.php
@@ -219,25 +219,66 @@ class ManagesTableSeeder extends Seeder
         //     ],
         // ]);
 
+        // // ジューシーズ ラボラトリー
+        // DB::table('manages')->insert([
+        //     [
+        //         'name' => 'ジューシーズ ラボラトリー',
+        //         'domain' => 'juicys',
+        //         'email' => 'nakanishi@lino-a.com',
+        //         'email_verified_at' => date('Y-m-d H:i:s'),
+        //         'tel' => '06-7508-5684',
+        //         'fax' => '0663471485',
+        //         'password' => Hash::make('e3XjPfaD'),
+
+        //         'delivery_shipping' => 0,
+        //         'delivery_preparation' => 60,
+        //         'delivery_sun' => '11:00,14:45,15:00,17:00',
+        //         'delivery_mon' => '11:00,14:45,15:00,17:00',
+        //         'delivery_wed' => '11:00,14:45,15:00,17:00',
+        //         'delivery_thu' => '11:00,14:45,15:00,17:00',
+        //         'delivery_fri' => '11:00,14:45,15:00,17:00',
+        //         'delivery_sat' => '11:00,14:45,15:00,17:00',
+
+        //         'created_at' => date('Y-m-d H:i:s'),
+        //         'updated_at' => date('Y-m-d H:i:s')
+        //     ],
+        // ]);
+        // DB::table('shops')->insert([
+        //     [
+        //         'manages_id' => 6,
+        //         'name' => '靱本町店',
+        //         'zipcode' => '550-0004',
+        //         'pref' => '大阪府',
+        //         'address1' => '大阪市西区靱本町２丁目',
+        //         'address2' => '６−１５ 1F',
+        //         'email' => 'nakanishi@lino-a.com',
+        //         'tel' => '06-7508-5684',
+        //         'fax' => '0675085684',
+
+        //         'created_at' => date('Y-m-d H:i:s'),
+        //         'updated_at' => date('Y-m-d H:i:s')
+        //     ],
+        // ]);
+
         // ジューシーズ ラボラトリー
         DB::table('manages')->insert([
             [
-                'name' => 'ジューシーズ ラボラトリー',
-                'domain' => 'juicys',
-                'email' => 'nakanishi@lino-a.com',
+                'name' => 'RUBBERSOUL',
+                'domain' => 'rubbersoul',
+                'email' => 'rubbersoul@lino-a.com',
                 'email_verified_at' => date('Y-m-d H:i:s'),
-                'tel' => '06-7508-5684',
+                'tel' => '06-6743−4565',
                 'fax' => '0663471485',
-                'password' => Hash::make('e3XjPfaD'),
+                'password' => Hash::make('GvHgjRKe'),
 
                 'delivery_shipping' => 0,
-                'delivery_preparation' => 60,
-                'delivery_sun' => '11:00,14:45,15:00,17:00',
-                'delivery_mon' => '11:00,14:45,15:00,17:00',
-                'delivery_wed' => '11:00,14:45,15:00,17:00',
-                'delivery_thu' => '11:00,14:45,15:00,17:00',
-                'delivery_fri' => '11:00,14:45,15:00,17:00',
-                'delivery_sat' => '11:00,14:45,15:00,17:00',
+                'delivery_preparation' => 45,
+                'delivery_sun' => '00:00,01:00,20:00,23:45',
+                'delivery_mon' => '00:00,01:00,20:00,23:45',
+                'delivery_wed' => '00:00,01:00,20:00,23:45',
+                'delivery_thu' => '00:00,01:00,20:00,23:45',
+                'delivery_fri' => '00:00,01:00,20:00,23:45',
+                'delivery_sat' => '00:00,01:00,20:00,23:45',
 
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
@@ -245,15 +286,15 @@ class ManagesTableSeeder extends Seeder
         ]);
         DB::table('shops')->insert([
             [
-                'manages_id' => 6,
+                'manages_id' => 7,
                 'name' => '靱本町店',
-                'zipcode' => '550-0004',
+                'zipcode' => '530-0003',
                 'pref' => '大阪府',
-                'address1' => '大阪市西区靱本町２丁目',
-                'address2' => '６−１５ 1F',
+                'address1' => '大阪市北区堂島',
+                'address2' => '1-4-26 玉家ビル B1F',
                 'email' => 'nakanishi@lino-a.com',
-                'tel' => '06-7508-5684',
-                'fax' => '0675085684',
+                'tel' => '06-6743−4565',
+                'fax' => '0667434565',
 
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
